d9c49666cd08c249b85929dd5f0bd3f256426a45
mihaisitaru <mihai.sitaru@yahoo.com>
2019-03-05 21:50:08 +0200

    'digPow'

--- script.js
+++ script.js
@@ -1,27 +1,54 @@
 function list(names) {
-  let rawString = '',
-      eachName,
-      finalString = '',
-      i = -1;
+  let rawString = "",
+    eachName,
+    finalString = "",
+    i = -1;
   names.forEach(n => {
     i++;
-    eachName = names.length - 2 === i ? n.name + ' & ' : eachName = n.name + ', ';
+    eachName =
+      names.length - 2 === i ? n.name + " & " : (eachName = n.name + ", ");
     rawString += eachName;
     finalString = rawString.substring(0, rawString.lastIndexOf(","));
   });
   return finalString;
 }
 
-console.log(list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}, {name: 'Mihai'} ]));
-console.log(list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Ana'} ]));
-console.log(list([ {name: 'Bart'} ]));
+console.log(
+  list([
+    { name: "Bart" },
+    { name: "Lisa" },
+    { name: "Maggie" },
+    { name: "Mihai" }
+  ])
+);
+console.log(list([{ name: "Bart" }, { name: "Lisa" }, { name: "Ana" }]));
+console.log(list([{ name: "Bart" }]));
 console.log(list([]));
 
-function highAndLow(numbers){
-  numbers = numbers.split(' ').sort((x, y) => x - y);
-  return numbers[numbers.length - 1] + ' ' + numbers[0];
+function highAndLow(numbers) {
+  numbers = numbers.split(" ").sort((x, y) => x - y);
+  return numbers[numbers.length - 1] + " " + numbers[0];
 }
 
 console.log(highAndLow("1 2 3 4 5"));
 console.log(highAndLow("1 2 -3 4 5"));
 console.log(highAndLow("1 9 3 4 -5"));
+
+function digPow(n, p) {
+  var x = 0,
+    i = -1,
+    array = n.toString().split("");
+
+  array.forEach(a => {
+    i++;
+    if (i < array.length) {
+      x += Math.pow(array[i], p);
+      p++;
+    }
+  });
+  return x % n ? -1 : x / n;
+}
+
+console.log(digPow(89, 1));
+console.log(digPow(92, 1));
+console.log(digPow(46288, 3));
